066741ad87c9959f7d481198b8db97b10b56161d
anetreba <anetreba@student.ucode.world>
2020-11-14 19:22:10 +0200

    Gameplay1

--- src/main/java/world/ucode/Avatar/Avatar.java
+++ src/main/java/world/ucode/Avatar/Avatar.java
@@ -3,8 +3,16 @@ package world.ucode.Avatar;
 public class Avatar {
     private String name;
     private Type type;
+    private double maxHealth = 10;
+    private double health = 10;
+    private double happiness = 10;
+    private double hunger = 10;
+    private double thirst = 10;
+    private double cleanliness = 10;
+
     public Avatar(Type type, String name) {
         this.type = type;
         this.name = name;
+        System.out.println("********************\n" + name + " " + toString() + "\n********************");
     }
 }
--- src/main/java/world/ucode/Controller/ControllerGamePlay.java
+++ src/main/java/world/ucode/Controller/ControllerGamePlay.java
@@ -1,5 +1,11 @@
 package world.ucode.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import world.ucode.Avatar.Avatar;
 
@@ -8,6 +14,83 @@ import java.util.ResourceBundle;
 
 public class ControllerGamePlay extends Controller {
     Avatar avatar;
+
+    @FXML
+    private ImageView AvatarView;
+
+    @FXML
+    private Button PlayButton;
+
+    @FXML
+    private Button GiveWaterButton;
+
+    @FXML
+    private Button GiveMedicineButton;
+
+    @FXML
+    private Button CleanUpButton;
+
+    @FXML
+    private Button BackGamePlay;
+
+    @FXML
+    private Label Name;
+
+    @FXML
+    private Label HungerLabel;
+
+    @FXML
+    private ProgressBar ProgressBarHunger;
+
+    @FXML
+    private Label ThirstLabel;
+
+    @FXML
+    private ProgressBar ProgressBarThirst;
+
+    @FXML
+    private Label CleanlinessLabel;
+
+    @FXML
+    private ProgressBar ProgressBarCleanliness;
+
+    @FXML
+    private Label HealthLabel;
+
+    @FXML
+    private ProgressBar ProgressBarHealth;
+
+    @FXML
+    private Label HappinessLabel;
+
+    @FXML
+    private ProgressBar ProgressBarHappiness;
+
+    @FXML
+    void HandleBackGamePlay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleCleanUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleGiveMedicine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleGiveWater(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandlePlay(ActionEvent event) {
+
+    }
+
 
     public ControllerGamePlay(Stage primaryStage, Avatar avatar) {
         super(primaryStage);
